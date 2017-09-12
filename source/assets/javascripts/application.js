11db38d0b2b10281c65eee22fdde28d749db785b
gmarinozzi <gmarinozzi@gmail.com>
2017-09-12 10:27:52 -0500

    header scroll

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,18 @@
 //= require_tree .
+jQuery(document).ready(function($) {
+
+    var navbar = $('.navbar-static-top'),
+    		distance = navbar.offset().top,
+        $window = $(window);
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > distance) {
+            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+          	$(".nav-bar li a").css("color", "#666");
+        } else {
+            navbar.removeClass('navbar-fixed-top');
+            $("body").css("padding-top", "0px");
+        }
+    });
+
+});
